a0ab590fb6bd45fe6bbc24b17437ee2bd70abece
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-10-03 12:47:12 -0700

    updating poms for 0.0.2-SNAPSHOT development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geometry "0.0.1"
+(defproject geometry "0.0.2-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
